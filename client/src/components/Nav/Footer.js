839a36dd23157c3e206a1f7c3db5c7c2148d4bb8
Akash2914 <akashdiomar@gmail.com>
2023-11-01 19:08:32 +0530

    First commit

--- client/src/components/Nav/Footer.js
+++ client/src/components/Nav/Footer.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { LinkedinOutlined } from '@ant-design/icons'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { LinkedinOutlined } from "@ant-design/icons";
 
 const Footer = () => {
   return (
     <div>
       <Container>
         <Row>
-          <Col className='text-center py-3'>
-            <Link to='/contactus' style={{ textDecoration: 'none' }}>
-              <h5 style={{ color: '#1881f0' }}>Contact Us</h5>
+          <Col className="text-center py-3">
+            <Link to="/contactus" style={{ textDecoration: "none" }}>
+              <h5 style={{ color: "#1881f0" }}>Contact Us</h5>
             </Link>
-              <p  style={{ color: '#1881f0',cursor:'pointer' }} onClick={()=>{
-                window.open('https://www.linkedin.com/in/sanjheev-r-505377198/','_blank','noopener','noreferrer')
-              }}>
-                <LinkedinOutlined />
-              </p>
+
+            <LinkedinOutlined />
           </Col>
         </Row>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const fs = require('fs')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+
+require("dotenv").config();
 
 //server
-const app = express()
+const app = express();
 
 //database
 mongoose
@@ -15,20 +17,35 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB CONNECTED'))
-  .catch((error) => console.log(`DB CONNECTION ERROR ${error}`))
+  .then(() => console.log("DB CONNECTED"))
+  .catch((error) => console.log(`DB CONNECTION ERROR ${error}`));
 
 //middlewares
-app.use(morgan('dev'))
-app.use(bodyParser.json({ limit: '2mb' }))
-app.use(cors())
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
 
+// deploy-------------------------------------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running Sucessfully");
+  });
+}
 //routes middleware
 
-fs.readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)))
+fs.readdirSync("./routes").map((r) =>
+  app.use("/api", require("./routes/" + r))
+);
 
 //port
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`SERVER IS RUNNING ON PORT ${port}`))
+app.listen(port, () => console.log(`SERVER IS RUNNING ON PORT ${port}`));
